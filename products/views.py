cb4356cab036c8b5ce53b3f153b34fd8680c1a48
unknown <peoplemany676@gmail.com>
2022-09-03 12:50:42 +0200

    add static

--- products/views.py
+++ products/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from .models import product
 
-# Create your views here.
+class ProductList(ListView):
+    model = product
+
+
