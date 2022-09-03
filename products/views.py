c8e62ff1a9b8595bc59a53cbaeadc5ddf58e3fa0
unknown <peoplemany676@gmail.com>
2022-09-03 23:04:41 +0200

    link static front-end2

--- products/views.py
+++ products/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from .models import product
 
 class ProductList(ListView):
     model = product
 
-
+class ProductDetail(DetailView):
+    model = product
