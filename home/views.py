6861b37908f9721b64aeccebb63cc620284f3914
unknown <peoplemany676@gmail.com>
2023-02-03 23:53:25 +0200

    edit home page

--- home/views.py
+++ home/views.py
@@ -1,11 +1,20 @@
 from django.shortcuts import render
-from products.models import Product, Brand
+from products.models import Product, Brand, Category, productReviews
 from .models import Baner
 
 # Create your views here.
 
 
 def home(request):
-    feature_products = Product.objects.filter(flag="Freature")[:6]
+    feature_products = Product.objects.filter(flag="Feature")[:6]
     new_products = Product.objects.filter(flag="New")[:6]
-    return render(request, 'home.html', {'products':feature_products})
+    sal_products = Product.objects.filter(flag="Sale")[:10]
+    brands = Brand.objects.all()
+    catefories = Category.objects.all()
+    reviews = productReviews.objects.all()
+    return render(request, 'home.html', {'feat_products':feature_products,
+    'new_product':new_products,
+    'sal_products':sal_products,
+    'brands':brands,
+    'category':catefories,
+    'productReviews':reviews})
